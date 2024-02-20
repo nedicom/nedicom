aad4f2dd5d52f2ccf294f044ce9079abe22e3ae6
Mario <you@example.com>
2024-02-20 18:36:59 +0300

    prices

--- resources/js/Pages/Uslugi/Usluga.vue
+++ resources/js/Pages/Uslugi/Usluga.vue
@@ -194,8 +194,11 @@ ol {
           <h3 class="text-4xl mx-12 my-1 font-semibold text-grey text-center">
             будем рады и Вашему отзыву
           </h3>
-          <p class="text-xs mx-12 font-semibold text-grey text-center md:text-end py-5">всего отзывов: {{reviewscount}}
-            </p>
+          <p
+            class="text-xs mx-12 font-semibold text-grey text-center md:text-end py-5"
+          >
+            всего отзывов: {{ reviewscount }}
+          </p>
           <Carousel v-bind="settings" :breakpoints="breakpoints">
             <Slide v-for="card in vars.reviews" :key="card">
               <div class="carousel__item w-full mx-3">
@@ -207,15 +210,66 @@ ol {
                     {{ card.created_at }}12.12.12
                   </div>
 
-                  <RatingReady :rating="card.rating"/>
+                  <RatingReady :rating="card.rating" />
 
                   <div class="flex items-center justify-start">
                     <div class="rounded-full w-12">
-                      <img
-                        :src="'https://nedicom.ru/'"
-                        width="120"
-                        class="rounded-full"
-                      />
+                      <svg v-if="card.id % 2"
+                        xmlns="http://www.w3.org/2000/svg"
+                        x="0px"
+                        y="0px"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 48 48"
+                      >
+                        <path
+                          fill="#fbc02d"
+                          d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+                        ></path>
+                        <path
+                          fill="#e53935"
+                          d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+                        ></path>
+                        <path
+                          fill="#4caf50"
+                          d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+                        ></path>
+                        <path
+                          fill="#1565c0"
+                          d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+                        ></path>
+                      </svg>
+
+                      <svg v-else
+                        xmlns="http://www.w3.org/2000/svg"
+                        x="0px"
+                        y="0px"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 48 48"
+                      >
+                        <linearGradient
+                          id="lpa7hSZqz_S376v76E9kia_wQ15B9zLAw61_gr1"
+                          x1="13.239"
+                          x2="37.906"
+                          y1="1.907"
+                          y2="33.479"
+                          gradientUnits="userSpaceOnUse"
+                        >
+                          <stop offset="0" stop-color="#f52537"></stop>
+                          <stop offset=".293" stop-color="#f32536"></stop>
+                          <stop offset=".465" stop-color="#ea2434"></stop>
+                          <stop offset=".605" stop-color="#dc2231"></stop>
+                          <stop offset=".729" stop-color="#c8202c"></stop>
+                          <stop offset=".841" stop-color="#ae1e25"></stop>
+                          <stop offset=".944" stop-color="#8f1a1d"></stop>
+                          <stop offset="1" stop-color="#7a1818"></stop>
+                        </linearGradient>
+                        <path
+                          fill="url(#lpa7hSZqz_S376v76E9kia_wQ15B9zLAw61_gr1)"
+                          d="M32,24h-7l8-18h7L32,24z M27,36.689	c0-4.168-0.953-8.357-2.758-12.117L15,6H8l10.833,21.169C20.251,30.123,21,33.415,21,36.689V42h6V36.689z"
+                        ></path>
+                      </svg>
                     </div>
                   </div>
 
