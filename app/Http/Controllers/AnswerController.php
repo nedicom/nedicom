07ffb5267f8ed2b1d23b448f00b3197d21de2462
Mario <you@example.com>
2024-08-16 13:05:34 +0300

    test

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -33,7 +33,7 @@ class AnswerController extends Controller
                 "url" => "https://nedicom.ru/questions/".$question->url,
                 "question" => $question->title,
             ];
-            Mail::to($user->user_mail)->send(new TestEmail($mailData));
+            Mail::to($user->email)->send(new TestEmail($mailData));
         }
     }
 
