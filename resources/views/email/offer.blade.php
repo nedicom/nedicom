e14c6983a70d6cc779d54fe1f5cf69b2e61cfd42
nedicom <m6132@yandex.ru>
2024-10-06 19:26:52 +0300

    mail offer

---
+++ resources/views/email/offer.blade.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Новое объявление</title>
+</head>
+<body>
+    <p>Пользователь: <em>{{ $mailData['user'] }}<em></p>
+    <p>Главная услуга: <em>{{ $mailData['main'] }}<em></p>
+    <p>Вторичная услуга: <em>{{ $mailData['second'] }}<em></p>
+    <p>Город: <em>{{ $mailData['city'] }}<em></p>
+    <p>Заголовок: <em>{{ $mailData['title'] }}<em></p>
+    <p>Доступно по ссылке: {{ $mailData['url'] }}</p>
+    
+    <blockquote>
+        <p class="has-line-data" data-line-start="26" data-line-end="33">
+            Отвечать на письмо не нужно. Оно формируется автоматически.
+        </p>
+    </blockquote>
+
+        <p>С уважением, команда ИП Мина О. В. (nedicom.ru @tm) Обратиться за юридической помощью можно на сайте https://nedicom.ru</p>
+    
+    <a target="_blank" href="https://nedicom.ru">
+        <img src="https://nedicom.ru/storage/images/landing/main/1280on600.webp" 
+        alt="" width="400" style="display:block">
+    </a>
+    
+</body>
+</html>
