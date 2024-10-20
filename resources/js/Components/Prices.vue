b7247d4e559db33c6f19e1adc795bdbd124ee610
nedicom <m6132@yandex.ru>
2024-10-20 21:46:12 +0300

    availability

--- resources/js/Components/Prices.vue
+++ resources/js/Components/Prices.vue
@@ -101,40 +101,40 @@ const prices = [
   <div itemscope itemtype="https://schema.org/Product" class="grid grid-cols-1 my-12 justify-items-center">
     <div class="grid grid-cols-2 gap-4 w-full md:w-1/2 mb-5 px-2">
       <div>
-        <img itemprop="image" :src="metaimage" :alt="subheader" class="rounded-lg"/>
+        <img itemprop="image" :src="metaimage" :alt="subheader" class="rounded-lg" />
       </div>
 
       <div>
-      <h2>
-        <span itemprop="name" class="text-3xl">{{ header }}</span> <span v-if="subheader"> в категории<br>
-          <span itemprop="name">
-            <span class="text-2xl">{{
-              subheader }}</span>
-            <span v-if="city">
-              по городу <span class="text-2xl">{{ city.title }}</span>
+        <h2>
+          <span itemprop="name" class="text-3xl">{{ header }}</span> <span v-if="subheader"> в категории<br>
+            <span itemprop="name">
+              <span class="text-2xl">{{
+                subheader }}</span>
+              <span v-if="city">
+                по городу <span class="text-2xl">{{ city.title }}</span>
+              </span>
             </span>
           </span>
-        </span>
-      </h2>
+        </h2>
 
-      <p itemprop="description" class="font-medium mt-5">
-        В категории {{
-          subheader }} <span v-if="city">по городу {{ city.title }} </span>{{ description }}
-      </p>
-      <p class="font-light mt-5">
-        {{ body }}
-      </p>
+        <p itemprop="description" class="font-medium mt-5">
+          В категории {{
+            subheader }} <span v-if="city">по городу {{ city.title }} </span>{{ description }}
+        </p>
+        <p class="font-light mt-5">
+          {{ body }}
+        </p>
 
-      <p itemprop="aggregateRating" itemscope itemtype="https://schema.org/AggregateRating"
-        class="text-xs mx-12 font-semibold text-grey text-center md:text-end py-5">
-        общая оценка:
-        <span v-if="rating" itemprop="ratingValue">{{ rating }}</span>
-        <span v-else itemprop="ratingValue">5</span>
-        всего отзывов:
-        <span v-if="reviewcoutnt" itemprop="reviewCount">{{ reviewcoutnt }}</span>
-        <span v-else itemprop="reviewCount">81</span>
-      </p>
-    </div>
+        <p itemprop="aggregateRating" itemscope itemtype="https://schema.org/AggregateRating"
+          class="text-xs mx-12 font-semibold text-grey text-center md:text-end py-5">
+          общая оценка:
+          <span v-if="rating" itemprop="ratingValue">{{ rating }}</span>
+          <span v-else itemprop="ratingValue">5</span>
+          всего отзывов:
+          <span v-if="reviewcoutnt" itemprop="reviewCount">{{ reviewcoutnt }}</span>
+          <span v-else itemprop="reviewCount">81</span>
+        </p>
+      </div>
     </div>
 
     <div class="w-full md:w-2/3 mb-5 px-2">
@@ -172,6 +172,37 @@ const prices = [
               </td>
               <td class="px-6 py-4">
                 *
+                <meta itemprop="availability" content="https://schema.org/InStock" />
+                
+                <div itemprop="shippingDetails" itemtype="https://schema.org/OfferShippingDetails" itemscope>
+                  <div itemprop="shippingRate" itemtype="https://schema.org/MonetaryAmount" itemscope>
+                    <meta itemprop="value" content="0" />
+                    <meta itemprop="currency" content="RUB" />
+                  </div>
+                  <div itemprop="shippingDestination" itemtype="https://schema.org/DefinedRegion" itemscope>
+                    <meta itemprop="addressCountry" content="RU" />
+                  </div>
+                  <div itemprop="deliveryTime" itemtype="https://schema.org/ShippingDeliveryTime" itemscope>
+                    <div itemprop="handlingTime" itemtype="https://schema.org/QuantitativeValue" itemscope>
+                      <meta itemprop="minValue" content="0" />
+                      <meta itemprop="maxValue" content="1" />
+                      <meta itemprop="unitCode" content="DAY" />
+                    </div>
+                    <div itemprop="transitTime" itemtype="https://schema.org/QuantitativeValue" itemscope>
+                      <meta itemprop="minValue" content="0" />
+                      <meta itemprop="maxValue" content="1" />
+                      <meta itemprop="unitCode" content="DAY" />
+                    </div>
+                  </div>
+                </div>
+
+                <div itemprop="hasMerchantReturnPolicy" itemtype="https://schema.org/MerchantReturnPolicy" itemscope>
+                  <meta itemprop="applicableCountry" content="RU" />
+                  <meta itemprop="returnMethod" content="https://schema.org/ReturnByMail" />
+                  <meta itemprop="returnFees" content="https://schema.org/FreeReturn" />
+                  <meta itemprop="returnPolicyCategory" content="https://schema.org/MerchantReturnFiniteReturnWindow" />
+                  <meta itemprop="merchantReturnDays" content="3" />
+                </div>
               </td>
             </tr>
           </tbody>
@@ -208,28 +239,7 @@ const prices = [
 
           </div>
 
-          <div itemprop="shippingDetails" itemtype="https://schema.org/OfferShippingDetails" itemscope>
-            <div itemprop="shippingRate" itemtype="https://schema.org/MonetaryAmount" itemscope>
-              <meta itemprop="value" content="0" />
-              <meta itemprop="currency" content="RUB" />
-            </div>
-            <div itemprop="shippingDestination" itemtype="https://schema.org/DefinedRegion" itemscope>
-              <meta itemprop="addressCountry" content="RU" />
-            </div>
-            <div itemprop="deliveryTime" itemtype="https://schema.org/ShippingDeliveryTime" itemscope>
-              <div itemprop="handlingTime" itemtype="https://schema.org/QuantitativeValue" itemscope>
-                <meta itemprop="minValue" content="0" />
-                <meta itemprop="maxValue" content="1" />
-                <meta itemprop="unitCode" content="DAY" />
-              </div>
-              <div itemprop="transitTime" itemtype="https://schema.org/QuantitativeValue" itemscope>
-                <meta itemprop="minValue" content="0" />
-                <meta itemprop="maxValue" content="1" />
-                <meta itemprop="unitCode" content="DAY" />
-              </div>
-            </div>
-
-          </div>
+          
 
           <div itemprop="hasMerchantReturnPolicy" itemtype="https://schema.org/MerchantReturnPolicy" itemscope>
             <meta itemprop="applicableCountry" content="RU" />
