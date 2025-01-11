7fbd584933cd2f33b92de0524e85defa751cd498
nedicom <m6132@yandex.ru>
2025-01-11 11:52:21 +0300

    search bar

--- app/Http/Controllers/MainpageController.php
+++ app/Http/Controllers/MainpageController.php
@@ -17,22 +17,28 @@ class MainpageController extends Controller
 {
 
     public function main(Request $request)
-    {        
+    {
         $reviewscount = Review::count();
         $rating = Review::sum('rating');
-        $rating =  round($rating / $reviewscount , 1);
+        $rating =  round($rating / $reviewscount, 1);
         $city = CitySet::CitySet($request, 0);
+
+        $secondoffers = Uslugi::where('is_second', 1)->where('is_feed', 1)->with('main')->select(['id', 'usl_name', 'url', 'file_path', 'main_usluga_id'])->get()
+        ->map(function ($item) {
+            $item->usl_name = $item->main->name . ' - ' .$item->usl_name;
+            $item->url = $item->main->url . '/' .$item->url;
+            return $item;
+        });;
 
         return Inertia::render('Welcome', [
             'city' => $city,
             'mainoffers' => Uslugi::where('is_main', 1)->where('is_feed', 1)->get(['id', 'usl_name', 'url', 'file_path']),
-            'secondoffers' => Uslugi::where('is_second', 1)->where('is_feed', 1)->get(['id', 'usl_name', 'url', 'file_path']),
+            'secondoffers' => $secondoffers,
             'practice' => Article::where('practice_file_path', '!=', null)->orderBy('updated_at', 'desc')->take(10)->get(),
             'reviews' => Review::inRandomOrder()->with('usluga')->get(),
             'reviewscount' => $reviewscount,
             'rating' => $rating,
-            'auth' => Auth::user(), 
+            'auth' => Auth::user(),
         ]);
     }
-    
 }
--- resources/js/Layouts/FrontQuestion.vue
+++ resources/js/Layouts/FrontQuestion.vue
@@ -7,6 +7,8 @@ let set = defineProps({
   city: String,
 });
 
+const alloffers = set.mainoffers.concat(set.secondoffers);
+
 let form = reactive({
   question: "",
 });
@@ -27,17 +29,12 @@ watch(
   (question) => {
     wordscounter.value = question.length;
     hidden.value = true;
-    //alert(set.mainoffers);
-    filterItems(set.mainoffers, question);
+    filterItems(alloffers, question);
     if (question.length > 1) {
       hidden.value = false;
     }
   }
 );
-
-function Selected(value) {
-  alert(value);
-}
 
 function Hide() {
   hidden.value = true;
@@ -54,8 +51,6 @@ let submit = () => {
 </script>
 
 <template>
-  <div @click="filterItems(searcharr, 'ap')">123</div>
-  {{ searcharr }}
   <form @submit.prevent="submit" class="flex justify-center mt-20">
     <div class="relative w-full md:w-1/3">
       <label
@@ -65,7 +60,7 @@ let submit = () => {
       >
       <div class="relative">
         <div
-          class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"
+          class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
         >
           <svg
             class="w-4 h-4 text-gray-500 dark:text-gray-400"
@@ -84,20 +79,19 @@ let submit = () => {
           </svg>
         </div>
         <input
-          :onBlur="Hide"
           v-model="form.question"
           type="search"
           id="search"
           autocomplete="off"
-          class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Какую услугу будем искать?"
           required
         />
         <button
           type="submit"
-          class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          поиск по услугам
+          найти
         </button>
       </div>
       <!-- SearchBox Dropdown -->
@@ -106,12 +100,12 @@ let submit = () => {
         class="absolute z-50 w-full bg-white border border-gray-200 rounded-lg"
       >
         <div
-          v-for="(item, num) in set.mainoffers"
-          :key="num"
+          v-for="(item, num) in searcharr"
+          :key="num"          
           @click="Selected(item.id)"
-          class="max-h-72 rounded-b-lg overflow-hidden overflow-y-auto m-2 cursor-pointer"
+          class="max-h-72 rounded-lg overflow-hidden overflow-y-auto m-1 p-1 cursor-pointer hover:bg-gray-200"
         >
-          {{ item.usl_name }}
+        <a :href="'/uslugi/' + city + '/' + item.url" >{{ item.usl_name }}</a>          
         </div>
       </div>
       <!-- End SearchBox Dropdown -->
