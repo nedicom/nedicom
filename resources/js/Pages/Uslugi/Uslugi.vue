16d318cdd3d618195be63fe6978e5ff5b3f7e8aa
Mario <you@example.com>
2025-02-04 17:02:48 +0300

    section

--- resources/js/Pages/Uslugi/Uslugi.vue
+++ resources/js/Pages/Uslugi/Uslugi.vue
@@ -311,6 +311,19 @@ function alertForm(x) {
 
     <Pagination v-if="uslugi.total > 10" :links="uslugi.links" />
 
+<Seodesc><section class="bg-white dark:bg-gray-900">
+    <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl md:grid md:grid-cols-2 md:py-16 md:px-6">
+        <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">We didn't reinvent the wheel</h2>
+            <p class="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
+            <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
+        </div>
+        <div class="mt-8">
+            <img class="w-full rounded-lg" src="https://nedicom.ru/storage/uslugi/1/mobile/1736932053usluga.webp" alt="office content 1">
+        </div>
+    </div>
+</section></Seodesc>
+
     <MainFooter>
       <BreadcrumbsUslugi
         class="p-4 bg-white flex flex-col items-center mb-2"
