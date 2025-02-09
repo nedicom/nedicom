64582a7d6f01018fe942ed5975418dffb710c3b0
nedicom <m6132@yandex.ru>
2025-02-09 20:20:06 +0300

    timezone

--- resources/views/sitemap/questions.blade.php
+++ resources/views/sitemap/questions.blade.php
@@ -4,7 +4,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
     @foreach ($questions as $item)
         <url>
             <loc>https://nedicom.ru/questions/{{ $item->url }}</loc>
-            <lastmod>{{ \Carbon\Carbon::createFromTimestamp(strtotime($item->getRawOriginal('created_at')))->format('Y-m-d\Th:i:sT') }}</lastmod>
+            <lastmod>{{ \Carbon\Carbon::createFromTimestamp(strtotime($item->getRawOriginal('created_at')))->format('Y-m-d\TH:i:sT') }}</lastmod>
             <changefreq>weekly</changefreq>
             <priority>0.9</priority>
         </url>
