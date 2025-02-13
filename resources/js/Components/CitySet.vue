23b9de275abc1e7a31f539e41cb73bf60824cdee
Mario <you@example.com>
2025-02-13 14:39:16 +0300

    set

--- resources/js/Components/CitySet.vue
+++ resources/js/Components/CitySet.vue
@@ -70,7 +70,6 @@ watch(
   <VueFinalModal class="flex justify-center items-center"
     content-class="flex flex-col max-w-xl mx-4 p-4 bg-white md:w-2/3 border dark:border-gray-700 rounded-lg space-y-2">
     <div class="relative">
-      {{ set.reloadpage }}
       <button type="button" @click="emit('confirm')"
         class="absolute top-0 right-0 bg-white rounded-md p-2 inline-flex items-end justify-end text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
         <span class="sr-only">Close menu</span>
