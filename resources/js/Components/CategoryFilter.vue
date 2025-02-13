3f97255218ca21f20e0ec821d289196b10a2634d
Mario <you@example.com>
2025-02-13 14:31:40 +0300

    menu

--- resources/js/Components/CategoryFilter.vue
+++ resources/js/Components/CategoryFilter.vue
@@ -3,15 +3,14 @@ import { ref } from "vue";
 
 const hideLinks = ref(true);
 
+const hideusl = ref(0);
+
 defineProps({
   category: Object,
   cityUrl: String,
   main_usluga_url: String,
   second_usluga_url: String,
 });
-
-defineEmits(['activeSts']);
-
 </script>
 
 <style scoped>
@@ -29,68 +28,128 @@ defineEmits(['activeSts']);
 
 <template>
   <div class="mx-2">
-    <h2 class="flex justify-center md:justify-end mb-2 md:mb-10">
-      <div @click="hideLinks = !hideLinks"
-        class="flex justify-center md:justify-end md:w-2/3 font-bold border-b md:border-none ">специализация юриста</div>
-      <button @click="hideLinks = !hideLinks" type="button"
-        class="flex items-center justify-between ml-4 font-medium rtl:text-right text-gray-500">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-        class="w-4 h-4 shrink-0">
-          <path stroke-linecap="round" stroke-linejoin="round"
-            d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
+    <h2 class="flex justify-center  mb-2 md:mb-10">
+      <div
+        @click="hideLinks = !hideLinks"
+        class="text-center md:text-right md:w-2/3 font-bold border-b md:border-none"
+      >
+        юристы
+      </div>
+      <button
+        @click="hideLinks = !hideLinks"
+        type="button"
+        class="flex items-center justify-between ml-4 font-medium text-gray-500"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          class="w-4 h-4 shrink-0"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"
+          />
         </svg>
-
       </button>
     </h2>
     <!-- Nested accordion -->
     <div :class="{ active: hideLinks, passive: !hideLinks }">
-      <div id="accordion-flush" data-accordion="collapse"
-        data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white"
-        data-inactive-classes="text-gray-500 dark:text-gray-400">
-        <div v-for="(u, numb) in category" :key="numb">
-          <h2 :id="'accordion-flush-heading-' + numb">
-            <div class="flex justify-start md:justify-end my-3 md:my-2 mx-3 md:mx-0 text-sm md:text-base font-semibold">
-              <a v-if="cityUrl" :href="route('offer.main', [cityUrl, u.url])"
-                class="flex justify-start md:justify-end w-full md:w-2/3">
-                <span class="flex justify-start md:justify-end w-full md:w-2/3 transition hover:-translate-x-1 duration-300 cursor-pointer text-right" :class="{ 'border-b-2 border-gray-300': u.url == main_usluga_url }">{{ u.usl_name }}</span>
-              </a>
-              <a v-else href="#" disabled @click="$emit('activeSts', true)"
-                class="flex justify-start md:justify-end p-1 text-gray-900 text-right group">
-                {{ u.usl_name }}
-              </a>
-              <button v-if="u.mainhassecond" type="button"
-                class="flex items-center justify-between ml-4 font-medium rtl:text-right text-gray-500"
-                :data-accordion-target="'#accordion-flush-body-' + numb" :aria-expanded="u.url == main_usluga_url"
-                :aria-controls="'accordion-flush-body-' + numb">
-                <svg v-if="u.mainhassecond.length > 0" data-accordion-icon class="w-3 h-3 text-gray-800 dark:text-white"
-                  aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="m19 9-7 7-7-7" />
-                </svg>
-                <svg v-else data-accordion-icon class="w-3 h-3 text-gray-800 dark:text-white" aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                </svg>
-              </button>
+      <div v-for="(u, numb) in category" :key="numb" class="mx-3">
+        <h2
+          class="flex justify-start md:justify-end my-3 md:my-2 mx-3 md:mx-0 text-sm xl:text-base font-semibold"
+        >
+          <a
+            v-if="cityUrl"
+            :href="route('offer.main', [cityUrl, u.url])"
+            class="flex justify-start md:justify-end w-full md:w-2/3 transition hover:-translate-x-1 duration-300 cursor-pointer text-right"
+          >
+            <span
+              :class="{
+                'underline  underline-offset-4 decoration-gray-700 m':
+                  u.url == main_usluga_url,
+              }"
+              >{{ u.usl_name }}
+            </span>
+          </a>
+          <a
+            v-else
+            href="#"
+            disabled
+            class="flex justify-start md:justify-end p-1 text-gray-900 text-right group"
+          >
+            {{ u.usl_name }}
+          </a>
+          <button
+            v-if="u.mainhassecond"
+            @click="hideusl = u.id"
+            type="button"
+            class="flex items-center justify-between ml-4 font-medium rtl:text-right text-gray-500"
+            :data-accordion-target="'#accordion-flush-body-' + numb"
+            :aria-expanded="u.url == main_usluga_url"
+            :aria-controls="'accordion-flush-body-' + numb"
+          >
+            <svg
+              v-if="u.mainhassecond.length > 0"
+              data-accordion-icon
+              class="w-3 h-3 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 9-7 7-7-7"
+              />
+            </svg>
+            <svg
+              v-else
+              data-accordion-icon
+              class="w-3 h-3 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            ></svg>
+          </button>
+        </h2>
 
-            </div>
-          </h2>
-
-          <!-- Subested accordion -->
-          <div v-if="u.mainhassecond" :id="'accordion-flush-body-' + numb" class="hidden mr-10"
-            :aria-labelledby="'accordion-flush-heading-' + numb">
-            <div v-if="u.mainhassecond.length > 0">
-              <span v-for="(second, secnumb) in u.mainhassecond" :key="secnumb">
-                <a v-if="cityUrl" :href="route('offer.second', [cityUrl, u.url, second.url])"
-                  class="flex justify-start md:justify-end transition hover:-translate-x-1 ml-5 md:ml-2 my-2 hover:ml-2 cursor-pointer text-sm md:text-base font-semibold md:font-normal text-gray-600">
-                  <span :class="{ 'border-b-2 border-gray-300': second.url == second_usluga_url }" class='md:text-right'>{{ second.usl_name
-                    }}</span></a>
-              </span>
-
-            </div>
+        <!-- Subested accordion -->
+        <div
+          v-if="u.mainhassecond"
+          class="relative overflow-hidden transition-all max-h-0 duration-700 ease-out pr-5"
+          :class="{
+            'max-h-96': u.id == hideusl,
+            invisible: !(u.id == hideusl),
+          }"
+        >
+          <div v-if="u.mainhassecond.length > 0">
+            <span v-for="(second, secnumb) in u.mainhassecond" :key="secnumb">
+              <a
+                v-if="cityUrl"
+                :href="route('offer.second', [cityUrl, u.url, second.url])"
+                class="flex justify-start md:justify-end transition hover:-translate-x-1 ml-5 md:ml-2 my-2 hover:ml-2 cursor-pointer text-sm xl:text-base font-semibold md:font-normal text-gray-600"
+              >
+                <span
+                  :class="{
+                    'underline  underline-offset-4 decoration-gray-600':
+                      second.url == second_usluga_url,
+                  }"
+                  class="md:text-right"
+                  >{{ second.usl_name }}</span
+                ></a
+              >
+            </span>
           </div>
-          <!-- Subested accordion -->
-
         </div>
+        <!-- Subested accordion -->
       </div>
     </div>
     <!-- Nested accordion -->
--- resources/js/Pages/Uslugi/Uslugi.vue
+++ resources/js/Pages/Uslugi/Uslugi.vue
@@ -56,11 +56,6 @@ title.value =
   set.city.title != ""
     ? title.value + " - " + set.city.title
     : title.value + " по всем городам";
-let status = ref(false);
-
-function alertForm(x) {
-  status.value = x;
-}
 
 const childRef = ref(null);
 
@@ -70,24 +65,34 @@ const callChildMethod = () => {
 </script>
 
 <template>
-
   <Head>
     <title>{{ title }}</title>
     <meta name="description" :content="description" />
   </Head>
 
   <div class="min-h-screen">
-    <MainHeader :auth="set.auth" :city="set.city" ref="childRef" :mainurl="set.main_usluga ? set.main_usluga.url : null"
-      :secondurl="set.second_usluga ? set.second_usluga.url : null" :reloadpage="true"/>
+    <MainHeader
+      :auth="set.auth"
+      :city="set.city"
+      ref="childRef"
+      :mainurl="set.main_usluga ? set.main_usluga.url : null"
+      :secondurl="set.second_usluga ? set.second_usluga.url : null"
+      :reloadpage="true"
+    />
 
     <Header :modalPageTitle="title" />
 
     <Body>
       <div class="bg-white grid grid-cols-1 md:grid-cols-4">
         <div>
-          <div class="flex max-w-xl justify-end items-center cursor-pointer my-5 mr-5 ">
-            <button @click="callChildMethod()" type="button"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5">
+          <div
+            class="flex max-w-xl justify-end items-center cursor-pointer my-5 mr-5"
+          >
+            <button
+              @click="callChildMethod()"
+              type="button"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
+            >
               <span v-if="set.city.title">{{ set.city.title }}</span>
               <span v-else>Выберите регион (город)</span>
             </button>
@@ -101,64 +106,115 @@ const callChildMethod = () => {
                 </p>
                 <div v-for="(allcity, n) in set.allsities" :key="n">
                   <h3 class="m-0">
-                    <a :href="route('uslugi.url', [allcity.cities.url])"
-                      class="bg-gray-100 text-xs rounded-md px-2 py-1">{{ allcity.cities.title }}</a>
+                    <a
+                      :href="route('uslugi.url', [allcity.cities.url])"
+                      class="bg-gray-100 text-xs rounded-md px-2 py-1"
+                      >{{ allcity.cities.title }}</a
+                    >
                   </h3>
                 </div>
               </div>
             </div>
           </div>
 
-          <CategoryFilter :category="set.category" :cityUrl="set.city.url"
-            :main_usluga_url="set.main_usluga.url ? set.main_usluga.url : '0'" :second_usluga_url="set.second_usluga ? set.second_usluga.url : 'false'
-              " @activeSts="alertForm" />
+          <CategoryFilter
+            :category="set.category"
+            :cityUrl="set.city.url"
+            :main_usluga_url="set.main_usluga.url ? set.main_usluga.url : '0'"
+            :second_usluga_url="
+              set.second_usluga ? set.second_usluga.url : 'false'
+            "
+          />
         </div>
-        <div class="w-full h-full col-span-3 md:pl-10 my-5 md:my-0" itemscope itemtype="https://schema.org/Product">
-          <meta v-if="set.second_usluga" itemprop="image"
-            :content="'https://nedicom.ru/' + set.second_usluga.file_path" />
-          <meta v-else itemprop="image" :content="'https://nedicom.ru/' + set.main_usluga.file_path" />
+        <div
+          class="w-full h-full col-span-3 md:pl-10 my-5 md:my-0"
+          itemscope
+          itemtype="https://schema.org/Product"
+        >
+          <meta
+            v-if="set.second_usluga"
+            itemprop="image"
+            :content="'https://nedicom.ru/' + set.second_usluga.file_path"
+          />
+          <meta
+            v-else
+            itemprop="image"
+            :content="'https://nedicom.ru/' + set.main_usluga.file_path"
+          />
 
           <div class="my-6 md:mb-0">
             <span>
               <h1
-                class="md:mb-4 flex md:justify-start justify-center text-sm leading-tight text-gray-900 dark:text-white md:text-2xl xl:text-2xl">
+                class="md:mb-4 flex md:justify-start justify-center text-sm leading-tight text-gray-900 dark:text-white md:text-2xl xl:text-2xl"
+              >
                 <span itemprop="name" class="font-bold">
-                  <a v-if="set.city.title != ''" :href="route('uslugi.url', [set.city.url])"
-                    class="font-medium text-blue-600 hover:underline">
-                    {{ set.city.title }}</a>
+                  <a
+                    v-if="set.city.title != ''"
+                    :href="route('uslugi.url', [set.city.url])"
+                    class="font-medium text-blue-600 hover:underline"
+                  >
+                    {{ set.city.title }}</a
+                  >
 
                   <span v-if="set.main_usluga && set.city.title != ''">
                     -
-                    <a v-if="set.main_usluga.url != 0" :href="route('offer.main', [set.city.url, set.main_usluga.url])
-                      " class="font-medium text-blue-600 hover:underline">
-                      {{ set.main_usluga.usl_name }}</a>
+                    <a
+                      v-if="set.main_usluga.url != 0"
+                      :href="
+                        route('offer.main', [set.city.url, set.main_usluga.url])
+                      "
+                      class="font-medium text-blue-600 hover:underline"
+                    >
+                      {{ set.main_usluga.usl_name }}</a
+                    >
                     <span v-else class="font-medium">
-                      {{ set.main_usluga.usl_name }}</span>
+                      {{ set.main_usluga.usl_name }}</span
+                    >
                   </span>
 
                   <span v-if="set.second_usluga && set.city.title != ''">
                     -
-                    <a :href="route('offer.second', [
-                      set.city.url,
-                      set.main_usluga.url,
-                      set.second_usluga.url,
-                    ])
-                      " class="font-medium">
-                      {{ set.second_usluga.usl_name }}</a>
+                    <a
+                      :href="
+                        route('offer.second', [
+                          set.city.url,
+                          set.main_usluga.url,
+                          set.second_usluga.url,
+                        ])
+                      "
+                      class="font-medium"
+                    >
+                      {{ set.second_usluga.usl_name }}</a
+                    >
                   </span>
                 </span>
               </h1>
-              <span v-if="set.second_usluga" itemprop="description"
-                class="font-bold flex md:justify-start justify-center">{{ set.second_usluga.usl_desc }}</span>
-              <span v-else itemprop="description" class="font-bold flex md:justify-start justify-center">{{
-                set.main_usluga.usl_desc }}</span>
+              <span
+                v-if="set.second_usluga"
+                itemprop="description"
+                class="font-bold flex md:justify-start justify-center"
+                >{{ set.second_usluga.usl_desc }}</span
+              >
+              <span
+                v-else
+                itemprop="description"
+                class="font-bold flex md:justify-start justify-center"
+                >{{ set.main_usluga.usl_desc }}</span
+              >
             </span>
 
-            <div v-if="set.sumrating !== 0 && set.countrating !== 0"
+            <div
+              v-if="set.sumrating !== 0 && set.countrating !== 0"
               class="mb-4 flex md:justify-start justify-center gap-2 text-sm font-medium text-gray-900"
-              itemprop="aggregateRating" itemscope itemtype="https://schema.org/AggregateRating">
+              itemprop="aggregateRating"
+              itemscope
+              itemtype="https://schema.org/AggregateRating"
+            >
               <span class="flex items-center" itemprop="ratingValue">
-                <RatingReady :rating="Number((set.sumrating / set.countrating).toFixed(2))" :reviewRating="false" />
+                <RatingReady
+                  :rating="Number((set.sumrating / set.countrating).toFixed(2))"
+                  :reviewRating="false"
+                />
               </span>
               из <span itemprop="bestRating">5.00</span> на основании
               <span itemprop="ratingCount">{{ set.countrating }} </span>отзывов
@@ -169,7 +225,10 @@ const callChildMethod = () => {
             <div v-if="set.uslugi.data[0]">
               <div
                 class="flex justify-start px-3 my-2 md:px-0 w-full md:w-4/5 text-sm font-medium text-gray-900 text-center"
-                itemprop="offers" itemscope itemtype="https://schema.org/AggregateOffer">
+                itemprop="offers"
+                itemscope
+                itemtype="https://schema.org/AggregateOffer"
+              >
                 Цены за консультацию
                 <meta itemprop="priceCurrency" content="RUB" />
                 от &nbsp;<span itemprop="lowPrice" content="1000">{{
@@ -179,12 +238,20 @@ const callChildMethod = () => {
                   {{ set.max }} рублей
                 </span>
                 &nbsp;у &nbsp;<span itemprop="offerCount">{{ set.count }}</span>
-                &nbsp;<span v-if="set.count == 1">юриста</span><span v-else>юристов</span>
+                &nbsp;<span v-if="set.count == 1">юриста</span
+                ><span v-else>юристов</span>
               </div>
               <!-- card -->
               <div v-for="offer in set.uslugi.data" :key="offer.id">
-                <OfferCard :offer="offer" :city="set.cities" :getlwr="set.getLawyer" :auth="set.auth" />
-                <hr class="h-px my-8 bg-gray-200 md:my-10 border-0 dark:bg-gray-700" />
+                <OfferCard
+                  :offer="offer"
+                  :city="set.cities"
+                  :getlwr="set.getLawyer"
+                  :auth="set.auth"
+                />
+                <hr
+                  class="h-px my-8 bg-gray-200 md:my-10 border-0 dark:bg-gray-700"
+                />
               </div>
               <!-- card -->
             </div>
@@ -193,17 +260,24 @@ const callChildMethod = () => {
               <!-- empty card -->
               <div class="flex flex-col justify-center my-10">
                 <p
-                  class="mb-4 mb-10 flex justify-center md:justify-start text-center md:text-left text-4xl tracking-tight font-extrabold text-gray-900">
+                  class="mb-4 mb-10 flex justify-center md:justify-start text-center md:text-left text-4xl tracking-tight font-extrabold text-gray-900"
+                >
                   Не нашел своего юриста? <br />
                   Мы поможем
                 </p>
                 <div class="flex justify-center md:justify-start">
-                  <a :href="route('questions.add')" цй
-                    class="rounded-lg flex inline-block bg-blue-700 px-6 py-3.5 text-center font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                    задать вопрос онлайн</a>
+                  <a
+                    :href="route('questions.add')"
+                    цй
+                    class="rounded-lg flex inline-block bg-blue-700 px-6 py-3.5 text-center font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  >
+                    задать вопрос онлайн</a
+                  >
                 </div>
               </div>
-              <hr class="h-px my-8 bg-gray-200 md:my-10 border-0 dark:bg-gray-700" />
+              <hr
+                class="h-px my-8 bg-gray-200 md:my-10 border-0 dark:bg-gray-700"
+              />
 
               <!-- empty card -->
             </div>
@@ -222,8 +296,11 @@ const callChildMethod = () => {
             <span class="flex-grow bg-gray-200 rounded h-0.5"></span>
           </div>
           <div v-for="(allcity, n) in set.allsities" :key="n">
-            <a :href="route('uslugi.url', [allcity.cities.url])" class="bg-gray-100 rounded-md px-3 py-2">{{
-              allcity.cities.title }}</a>
+            <a
+              :href="route('uslugi.url', [allcity.cities.url])"
+              class="bg-gray-100 rounded-md px-3 py-2"
+              >{{ allcity.cities.title }}</a
+            >
           </div>
         </div>
       </div>
@@ -238,10 +315,13 @@ const callChildMethod = () => {
     <CtaLwr v-if="true" />
 
     <MainFooter>
-      <BreadcrumbsUslugi class="p-4 bg-white flex flex-col items-center mb-2"
+      <BreadcrumbsUslugi
+        class="p-4 bg-white flex flex-col items-center mb-2"
         :city="set.city.title !== '' ? set.city : null"
         :main_usluga="set.main_usluga.url !== 0 ? set.main_usluga : null"
-        :second_usluga="set.second_usluga ? set.second_usluga : null" :usluga="null" />
+        :second_usluga="set.second_usluga ? set.second_usluga : null"
+        :usluga="null"
+      />
     </MainFooter>
   </div>
 </template>
