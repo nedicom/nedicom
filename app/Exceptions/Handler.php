7f9dceb781fbb859ae8ad75c410271635b5e2aaf
nedicom <m6132@yandex.ru>
2025-03-13 15:24:58 +0300

    dd($response);

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -59,6 +59,8 @@ class Handler extends ExceptionHandler
     {
         $response = parent::render($request, $e);
 
+        dd($response);
+        
         if (!app()->environment('local') && in_array($response->status(), [500, 503, 404, 403, 410])) {
             return Inertia::render('Errors/Errors', ['status' => $response->status()])
                 ->toResponse($request)
@@ -68,7 +70,7 @@ class Handler extends ExceptionHandler
                 'message' => 'Срок действия страницы истек, попробуйте еще раз.',
             ]);
         }
-dd($response);
+
         return $response;
     }
 }
