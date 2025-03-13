3b1099386d4d97742dd85be596dad356e8edb18e
nedicom <m6132@yandex.ru>
2025-03-13 15:34:49 +0300

    test

--- app/Helpers/CitySet.php
+++ app/Helpers/CitySet.php
@@ -48,22 +48,20 @@ class CitySet
     {
 
         $ip = GetIp::get_ip();
-        //dd($ip);
         $token = "4667f5f8016f7069789e94041a49052dd82414a3";
         $secret = "9d6b5f1aabde129507b11950dbdf272716443d54";
         $dadata = new \Dadata\DadataClient($token, $secret);
-        $result = $dadata->iplocate('qwery');
+        $result = $dadata->iplocate($ip);
+
         if ($result) {
-            dd($result['data']['postal_code']);
+            $city = cities::where('postalcode', $result['data']['postal_code'])->first();
         }
 
-
-        $city = cities::find(0);
-
+        if (!$city) {
+            $city = cities::find(0);
+        }
         return $city;
-
-
-
+        
         if ($cityid) {
             $city = cities::where('id', $cityid)->first();
             session(['cityid' => $city->id, 'citytitle' => $city->title]);
