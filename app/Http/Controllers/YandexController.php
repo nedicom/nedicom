36bc0f24c9aba737d27633da0ef504e56121b435
nedicom <m6132@yandex.ru>
2025-05-08 15:02:42 +0300

    test

--- app/Http/Controllers/YandexController.php
+++ app/Http/Controllers/YandexController.php
@@ -16,13 +16,40 @@ class YandexController extends Controller
     public function yandexoauthsucces()
     {
        
-       /*return response(<<<HTML
-        <script>
-            // Если нет родительского окна - редирект
-            window.close();
-    </script>
-    HTML);
-    */
+        try {
+            // 1. Получаем access token по коду
+            $response = Http::asForm()->post(config('services.yandex.token_url'), [
+                'grant_type' => 'authorization_code',
+                'code' => request('code'),
+                'client_id' => config('services.yandex.client_id'),
+                'client_secret' => config('services.yandex.client_secret'),
+            ]);
+
+
+            if (!$response->ok()) {
+                throw new \Exception('Failed to get access token');
+            }
+
+            $accessToken = $response->json()['access_token'];
+
+            // 2. Получаем информацию о пользователе
+            $userInfo = Http::withHeaders([
+                'Authorization' => 'OAuth ' . $accessToken,
+            ])->get(config('services.yandex.user_info_url'), [
+                'format' => 'json',
+            ])->json();
+
+            // 3. Создаем или обновляем пользователя
+            $user = User::updateOrCreate([
+                'email' => $userInfo['default_email'],
+            ], [
+                'name' => $userInfo['real_name'] ?? $userInfo['login'],
+                'yandex_id' => $userInfo['id'],
+                //'password' => bcrypt(Str::random(16)), // Генерируем случайный пароль
+            ]);
+
+            // 4. Авторизуем пользователя
+            Auth::login($user);
     }
 
 
