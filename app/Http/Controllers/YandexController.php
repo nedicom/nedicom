5bc44b73769ba480f8d05172dfd7fe43d58ac984
nedicom <m6132@yandex.ru>
2025-05-08 13:58:47 +0300

    redirect_uri

--- app/Http/Controllers/YandexController.php
+++ app/Http/Controllers/YandexController.php
@@ -51,7 +51,7 @@ class YandexController extends Controller
             // 4. Авторизуем пользователя
             Auth::login($user);
 
-
+/*
             return response(<<<HTML
                 <script>
                 if (window.opener) {
@@ -74,7 +74,7 @@ class YandexController extends Controller
             HTML);
 
 
-            /*
+            
             if (Cookie::get('last_url')) {
                 return redirect()->to(Cookie::get('last_url'));
             } else {
