ac3fbdf687e8d1760ccd38e0f161694835525e3c
nedicom <m6132@yandex.ru>
2025-05-08 13:03:39 +0300

    accessToken

--- app/Http/Controllers/YandexController.php
+++ app/Http/Controllers/YandexController.php
@@ -7,11 +7,29 @@ use App\Http\Controllers\Controller;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Facades\Http;
 
 class YandexController extends Controller
 {
     public function yandexoauth()
     {
+
+        $response = Http::asForm()->post(config('services.yandex.token_url'), [
+            'grant_type' => 'authorization_code',
+            'code' => request('code'),
+            'client_id' => config('services.yandex.client_id'),
+            'client_secret' => config('services.yandex.client_secret'),
+        ]);
+
+
+        if (!$response->ok()) {
+            throw new \Exception('Failed to get access token');
+        }
+
+        $accessToken = $response->json()['access_token'];
+
+        dd($accessToken);
+
         $user = Socialite::driver('yandex')->user();
 
         // Ваша логика создания/авторизации пользователя
