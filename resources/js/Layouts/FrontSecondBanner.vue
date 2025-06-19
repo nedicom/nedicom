9e98bd439f02c4be37dbb4492f6449e14bb6b00f
Mario <you@example.com>
2025-06-19 13:20:06 +0300

    tel

--- resources/js/Layouts/FrontSecondBanner.vue
+++ resources/js/Layouts/FrontSecondBanner.vue
@@ -53,7 +53,7 @@ const props = defineProps({
         class="px-5 flex flex-col lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4"
       >
         <a
-          href="#"
+          href="tel:+79788838978"
           class="inline-flex justify-center items-center py-5 px-5 text-4xl font-bold text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
         >
           89788838978
