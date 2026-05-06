c2482bd7193cca1cd70ec83a1890528385c21996
nedicom <m6132@yandex.ru>
2026-05-06 10:43:17 +0300

    Roistat Counter

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -97,23 +97,16 @@
     </script>
     <!-- Fonts to page speed -->
 
-    <!-- Roistat Counter Start -->
-    <script>
-        (function(w, d, s, h, id) {
-            w.roistatProjectId = id;
-            w.roistatHost = h;
-            w.roistatPage = d.location.href;
-            w.roistatReferrer = d.referrer;
-            var p = d.location.protocol == "https:" ? "https://" : "http://";
-            var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/" + id + "/init?referrer=" + encodeURIComponent(d.location.href);
-            var js = d.createElement(s);
-            js.charset = "UTF-8";
-            js.async = 1;
-            js.src = p + h + u;
-            var js2 = d.getElementsByTagName(s)[0];
-            js2.parentNode.insertBefore(js, js2);
-        })(window, document, 'script', 'cloud.roistat.com ', '7a56072f212362140ba2f651690ec0f4');
-    </script> <!-- Roistat Counter End -->
+<!-- Roistat Counter Start -->
+<script>
+(function(w, d, s, h, id) {
+    w.roistatProjectId = id; w.roistatHost = h; w.roistatPage = d.location.href; w.roistatReferrer = d.referrer;
+    var p = d.location.protocol == "https:" ? "https://" : "http://";
+    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);
+    var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+})(window, document, 'script', 'cloud.roistat.com', '7a56072f212362140ba2f651690ec0f4');
+</script>
+<!-- Roistat Counter End -->
 
 </body>
 
